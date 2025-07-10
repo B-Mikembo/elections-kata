78910fd9a52264160a8e8505b12e363d8bdb16ff
B-Mikembo <mikembo.brice.pro@gmail.com>
2025-07-10 07:58:33 +0200

    fractionner boucle

--- java/src/main/java/org/elections/ElectionsWithoutDistrict.java
+++ java/src/main/java/org/elections/ElectionsWithoutDistrict.java
@@ -25,36 +25,13 @@ public class ElectionsWithoutDistrict extends Elections {
 
     @Override
     public Map<String, String> results() {
-        Map<String, String> results = new HashMap<>();
-        Integer nbVotes = 0;
-        Integer nullVotes = 0;
-        Integer blankVotes = 0;
-        int nbValidVotes = 0;
+        int nbVotes = votesWithoutDistricts.stream().reduce(0, Integer::sum);
+        var results = totalForEachOfficialCandidate();
 
-        nbVotes = votesWithoutDistricts.stream().reduce(0, Integer::sum);
-        for (int i = 0; i < officialCandidates.size(); i++) {
-            int index = candidates.indexOf(officialCandidates.get(i));
-            nbValidVotes += votesWithoutDistricts.get(index);
-        }
-
-        for (int i = 0; i < votesWithoutDistricts.size(); i++) {
-            Float candidatResult = ((float) votesWithoutDistricts.get(i) * 100) / nbValidVotes;
-            String candidate = candidates.get(i);
-            if (officialCandidates.contains(candidate)) {
-                results.put(candidate, String.format(Locale.FRENCH, "%.2f%%", candidatResult));
-            } else {
-                if (candidates.get(i).isEmpty()) {
-                    blankVotes += votesWithoutDistricts.get(i);
-                } else {
-                    nullVotes += votesWithoutDistricts.get(i);
-                }
-            }
-        }
-
-        float blankResult = ((float) blankVotes * 100) / nbVotes;
+        float blankResult = ((float) totalBlankVote() * 100) / nbVotes;
         results.put("Blank", String.format(Locale.FRENCH, "%.2f%%", blankResult));
 
-        float nullResult = ((float) nullVotes * 100) / nbVotes;
+        float nullResult = ((float) totalNullVote() * 100) / nbVotes;
         results.put("Null", String.format(Locale.FRENCH, "%.2f%%", nullResult));
 
         int nbElectors = list.values().stream().map(List::size).reduce(0, Integer::sum);
@@ -64,5 +41,53 @@ public class ElectionsWithoutDistrict extends Elections {
         results.put("Abstention", String.format(Locale.FRENCH, "%.2f%%", abstentionResult));
 
         return results;
+    }
+
+    private Integer totalNullVote() {
+        var nullVotes = 0;
+        for (int i = 0; i < votesWithoutDistricts.size(); i++) {
+            String candidate = candidates.get(i);
+            if (!officialCandidates.contains(candidate)) {
+                if (!candidates.get(i).isEmpty()) {
+                    nullVotes += votesWithoutDistricts.get(i);
+                }
+            }
+        }
+        return nullVotes;
+    }
+
+    private Integer totalBlankVote() {
+        var blankVotes = 0;
+        for (int i = 0; i < votesWithoutDistricts.size(); i++) {
+            String candidate = candidates.get(i);
+            if (!officialCandidates.contains(candidate)) {
+                if (candidates.get(i).isEmpty()) {
+                    blankVotes += votesWithoutDistricts.get(i);
+                }
+            }
+        }
+        return blankVotes;
+    }
+
+    private Map<String, String> totalForEachOfficialCandidate() {
+        var results = new HashMap<String, String>();
+        var nbValidVotes = totalValidVote();
+        for (int i = 0; i < votesWithoutDistricts.size(); i++) {
+            Float candidatResult = ((float) votesWithoutDistricts.get(i) * 100) / nbValidVotes;
+            String candidate = candidates.get(i);
+            if (officialCandidates.contains(candidate)) {
+                results.put(candidate, String.format(Locale.FRENCH, "%.2f%%", candidatResult));
+            }
+        }
+        return results;
+    }
+
+    private int totalValidVote() {
+        var nbValidVotes = 0;
+        for (String officialCandidate : officialCandidates) {
+            int index = candidates.indexOf(officialCandidate);
+            nbValidVotes += votesWithoutDistricts.get(index);
+        }
+        return nbValidVotes;
     }
 }
